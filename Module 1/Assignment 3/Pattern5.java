50226266bd17baced720edf3a0663c972a317bc1
sankupatil <100894878+sankupatil@users.noreply.github.com>
2022-03-31 15:20:59 +0530

    Add files via upload

---
+++ Module 1/Assignment 3/Pattern5.java
@@ -0,0 +1,15 @@
+class Pattern5
+{
+	public static void main(String args[])
+	{
+		for (char i=65; i<=70; i++)
+		{
+			for (char j=65; j<=i; j++)
+			{
+			System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
